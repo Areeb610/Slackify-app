208a915d5ff4bc0a8a7ab2908d8badf8a605a4af
rohanasif1990 <rohanasif1990@gmail.com>
2023-07-11 15:17:45 +0500

    header complete

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import Login from './Login'
-import Signup from './Signup'
+import React from "react";
+import Login from "./Login";
+import Signup from "./Signup";
+import Company from "./Company";
 
 const Homepage = () => {
   return (
-    <div><Signup/></div>
-  )
-}
+    <div>
+      <Company />
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
