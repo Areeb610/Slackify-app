7db550a193f3792989c68476e4f9807ed12cb0b2
rohanasif1990 <rohanasif1990@gmail.com>
2023-07-11 12:32:30 +0500

    Add font awesome deps

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
-import './App.css'
-import {Route, BrowserRouter} from 'react-router-dom';
-import Homepage from './pages/Homepage';
-import Chatspage from './pages/Chatspage';
-import Login from './pages/Login';
+import "./App.css";
+import { Route, BrowserRouter } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Chatspage from "./pages/Chatspage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={Login} exact />
+      <Route path="/" component={Homepage} exact />
       <Route path="/chats" component={Chatspage} />
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
