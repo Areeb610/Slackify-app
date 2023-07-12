7f700a6d1ce49de994af65b7fcdf6fbc6a7ef653
Faizan Khalid <faizankhalid515@gmail.com>
2023-07-12 15:38:18 +0500

    get people in workspace api

--- backend/controller/workspace.js
+++ backend/controller/workspace.js
@@ -25,3 +25,27 @@ export const create_workspace = async (req, res) => {
     }
 
 }
+
+export const people_in_workspace = async (req, res) => {
+    try {
+        const {workspace_id} = req.body;
+        if (!workspace_id) {
+            return res.status(404).send({success: false, message: "No Workspace Found"});
+        } else {
+            const workspace = client.query(`Select person.id, person.name from people_in_workspace join person on people_in_workspace.added_people = person.id
+              where people_in_workspace.workspace_id = ${workspace_id}`, (error, result) => {
+                if (!error && result.rowCount > 0) {
+                    return res.status(200).send({success: true, data: result.rows});
+                } else if (!error && result.rowCount == 0) {
+                    return res.status(200).send({success: false, message: "No Workspace or People Found"});
+                } else {
+                    return res.status(404).send({success: false, message: "Error in Getting Workspace Friends", error});
+                }
+            })
+        }
+
+    } catch (err) {
+        return res.status(500).send({success: false, message: "Error in Getting Workspace Friends", err});
+    }
+
+}
--- backend/routes/uroutes.js
+++ backend/routes/uroutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {registercontroller, logincontroller} from "../controller/usercontroller.js";
-import {create_workspace} from "../controller/workspace.js";
+import {create_workspace, people_in_workspace} from "../controller/workspace.js";
 import {get_invite, invite_people, accept_invite} from "../controller/invite.js";
 
 
@@ -17,6 +17,8 @@ router.post("/invite_people", invite_people)
 router.get("/get_invite", get_invite)
 
 router.put("/accept_invite", accept_invite)
+
+router.get("/get_people", people_in_workspace)
 
 
 export default router;
