624e7cee126362bf50f94cbf7820a8e843354596
Salman <salman.ashraff20@gmail.com>
2023-07-12 17:49:18 +0500

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,15 @@ import Homepage from "./pages/Homepage";
 import Chatspage from "./pages/Chatspage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-
+import SideBar from "./pages/SideBar";
 function App() {
   return (
     <BrowserRouter>
       <Route path="/" component={Homepage} exact />
       <Route path="/chats" component={Chatspage} />
+      <Route path="/sidebar" component={SideBar} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
     </BrowserRouter>
   );
 }
