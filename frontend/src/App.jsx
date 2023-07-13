e5fbe889368053886803d0b93feafe68e73b6097
Areeb610 <areebbutt610@gmail.com>
2023-07-13 17:26:53 +0500

    updated App.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import './App.css'
 import {Route, BrowserRouter} from 'react-router-dom';
 import Chatspage from './pages/Chatspage';
-import Login from './pages/Login';
+import Login from '../src/components/Login'
 import NewWorkspace from './pages/NewWorkspace';
 import SideBar from './pages/SideBar';
-import Signup from './pages/Signup';
-import MainPage from './pages/MainPage';
-import Login from './components/Login';
+import Signup from '../src/components/Signup';
+import MainPage from './pages/MainPage'
 import Company from './pages/Company'
 
 function App() {
   return (
     <div className="App">
     <BrowserRouter >
-      <Route path="/" component={MainPage} />
-      <Route path="/" component={NewWorkspace} exact />
+      <Route path="/" component={Login} />
+      <Route path="/addworkspace" component={NewWorkspace} exact />
       <Route path="/chats" component={Chatspage} />
       <Route path="/sidebar" component={SideBar} />
       <Route path="/login" component={Login} />
