9908bd820de4e57299b3747d3bd87f10660ea942
rohanasif1990 <rohanasif1990@gmail.com>
2023-07-14 11:54:59 +0500

    remove unnecessary routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import { Route, BrowserRouter } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
-import Chatspage from "./pages/Chatspage/Chatspage";
 import Login from "./pages/Login/Login";
-import SideBar from "./components/SideBar";
 import Signup from "./pages/SignUp/Signup";
 import MainPage from "./pages/MainPage/MainPage";
 
@@ -11,8 +9,6 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Route path="/" component={Homepage} exact />
-        <Route path="/chats" component={Chatspage} />
-        <Route path="/sidebar" component={SideBar} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/mainpage" component={MainPage} />
